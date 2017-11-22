6e185db8890cfc19ee5a9488840300f72558dbe7
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2017-11-22 16:36:36 -0600

    worked on inputs and object constructor

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,18 @@ console.log('JS sourced');
 $(document).ready( function () {
     console.log('jQ sourced');
     
+    $('#addButton').on('click', function () {
+        console.log('in addbutton');
+        //creates object to be set to DB
+        var objectToSend = {
+            task: $('#taskIn').val(),
+            description: $('#descriptionIn').val(),
+            dueDate: $('#dueDateIn').val(),
+            completed: 'N'
+        }
+        console.log(objectToSend);
+        
+    })
 })
+
+
