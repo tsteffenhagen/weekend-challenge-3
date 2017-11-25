c77468a6db50dde4dd58ec4305cfaaf45218c32e
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2017-11-24 23:34:22 -0600

    Updated functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,12 @@
 console.log('JS sourced');
 
-$(document).ready( function () {
+$(document).ready(function () {
     console.log('jQ sourced');
-    
+    getToDoList(); //gets pre-existing list items on load
+
     $('#addButton').on('click', function () {
         console.log('in addbutton');
-        //creates object to be set to DB
+        //creates object to be sent to DB
         var objectToSend = {
             task: $('#taskIn').val(),
             description: $('#descriptionIn').val(),
@@ -13,8 +14,115 @@ $(document).ready( function () {
             completed: 'N'
         }
         console.log(objectToSend);
-        
+        addListItem(objectToSend);//sends object to the server
+    })//End addButton on click
+
+    $('#viewToDoList').on('click', '.deleteButton', deleteListItem);
+
+    $('#viewToDoList').on('click', '.completeButton', completeListItem);
+    
+    $('#viewToDoList').on('click', '.lateCompleteButton', completeListItem);
+
+})//End Document on ready
+
+
+
+function addListItem(newListItem) {
+    console.log('in add list item: ', newListItem);
+
+    $.ajax({
+        url: '/todo/',
+        method: 'POST',
+        data: newListItem,
+        success: function (data) {
+            console.log('got list items, ', data);
+            getToDoList();
+        }
     })
-})
+}//end addListItem function
 
+function getToDoList() {
+    var rightNow = new Date()
+    var res = rightNow.toISOString().slice(0,10).replace(/-/g,"-");
+    console.log(rightNow + "  " + res);
+    
 
+    console.log('in getToDoList');
+    $.ajax({
+        url: '/todo/',
+        method: 'GET',
+        success: function (response) {
+            console.log('List Items: ', response);
+            $('#viewToDoList').html('');
+            for (let i = 0; i < response.length; i++) {
+                var listItem = response[i];
+                var longDate = listItem.due_date;
+                var trimmedDate = longDate.substring(0, 10);
+                console.log(res, " ", trimmedDate);
+                
+                if (listItem.completed == 'Y') {
+                    var $newListItem = $('<tr><td>' + listItem.task + '</td><td>' + listItem.description + '</td><td>' + trimmedDate + '</td><td>' + listItem.completed + '</td></tr>');
+                    var $deleteListItemButton = $('<td><button class = "deleteButton">Delete</button></td>');
+                    var $completeListItemButton = $('<td><button class = "completedButton">Way To Go!!!</button></td>');
+                    $deleteListItemButton.data('id', listItem.user_id);
+                    $completeListItemButton.data('id', listItem.user_id);
+                    $newListItem.append($deleteListItemButton);
+                    $newListItem.append($completeListItemButton);
+                    $('#viewToDoList').append($newListItem)
+                    $('.completedButton').parent().parent().css("background-color", "green");
+                } else if (res >= trimmedDate) {
+                    var $newListItem = $('<tr><td>' + listItem.task + '</td><td>' + listItem.description + '</td><td>' + trimmedDate + '</td><td>' + listItem.completed + '</td></tr>');
+                    var $deleteListItemButton = $('<td><button class = "deleteButton">Delete</button></td>');
+                    var $completeListItemButton = $('<td><button class = "lateCompleteButton">Complete past due item</button></td>');
+                    $deleteListItemButton.data('id', listItem.user_id);
+                    $completeListItemButton.data('id', listItem.user_id);
+                    $newListItem.append($deleteListItemButton);
+                    $newListItem.append($completeListItemButton);
+                    $('#viewToDoList').prepend($newListItem)
+                    $('.lateCompleteButton').parent().parent().css("background-color", "red");
+                } else {
+                    var $newListItem = $('<tr><td>' + listItem.task + '</td><td>' + listItem.description + '</td><td>' + trimmedDate + '</td><td>' + listItem.completed + '</td></tr>');
+                    var $deleteListItemButton = $('<td><button class = "deleteButton">Delete</button></td>');
+                    var $completeListItemButton = $('<td><button class = "completeButton">Item Completed!!!</button></td>');
+                    $deleteListItemButton.data('id', listItem.user_id);
+                    $completeListItemButton.data('id', listItem.user_id);
+                    $newListItem.append($deleteListItemButton);
+                    $newListItem.append($completeListItemButton);
+                    $('#viewToDoList').prepend($newListItem)
+                }
+            }
+        }
+    })
+}//end getToDoList function
+
+function deleteListItem() {
+    console.log($(this).parent().data('id'));
+
+    var $listItemIdToRemove = $(this).parent().data('id');
+    console.log('The list item ID to be removed is: ', $listItemIdToRemove);
+    var r = confirm("Are you sure you want to delete?");
+    if (r == true) {
+    $.ajax({
+        url: '/todo/' + $listItemIdToRemove,
+        method: 'DELETE',
+        success: function (response) {
+            getToDoList();
+        }
+    });} else {
+        console.log('Item not deleted');        
+    }
+
+}//end deleteListItem function
+
+function completeListItem() {
+    console.log($(this).parent().data('id'));
+    var $listItemIdToRemove = $(this).parent().data('id');
+
+    $.ajax({
+        url: '/todo/' + $listItemIdToRemove,
+        method: 'PUT',
+        success: function (response) {
+            getToDoList();
+        }
+    })
+}//end completeListItem
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,4 +3,85 @@ var router = express.Router();
 var pg = require('pg');
 var pool = require('../modules/pool.js');
 
+router.post('/', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database:', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`INSERT INTO list (task, description, due_date, completed)
+            VALUES ($1, $2, $3, $4);`, [req.body.task, req.body.description, req.body.dueDate, req.body.completed],
+                function (errorMakingQuery, result) {
+                    done();
+                    if (errorMakingQuery) {
+                        res.sendStatus(500);
+                    } else {
+                        res.sendStatus(201);
+                    }
+                });
+        }
+    })
+});//end router.post
+
+router.get('/', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query('SELECT * FROM list ORDER BY due_date DESC;', function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making query:', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            })
+        }
+    })
+}); //end router.get
+
+router.delete('/:id', function (req, res) {
+    var listItemIdToRemove = req.params.id;
+
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query('DELETE FROM list WHERE user_id=$1;', [listItemIdToRemove], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            })
+        }
+    })
+});//end router.delete
+
+router.put('/:id', function (req, res) {
+    var listItemIdToChange = req.params.id;
+
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`UPDATE list SET completed = 'Y' WHERE user_id=$1;`, [listItemIdToChange], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        }
+    })
+});// end router.put
+
 module.exports = router;
